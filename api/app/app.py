37425d07a8b8ebcd0af7db82a4d6a8b189e0ec48
matvey <matvey.lipilin1@gmail.com>
2023-11-23 23:17:06 +0500

    feat: add theme_group from theme

--- api/app/app.py
+++ api/app/app.py
@@ -2,7 +2,13 @@ from fastapi import FastAPI
 
 from app.classifier.tfidf import TfidfClassifier
 from app.data_models.prediction import Prediction
+from app.theme_mapping import THEME_TO_GROUP
 from resources import RESOURCES_PATH
+
+
+def enrich_theme_group(prediction: Prediction) -> None:
+    if prediction.theme_group is None:
+        prediction.theme_group = THEME_TO_GROUP.get(prediction.theme)
 
 
 def build_app() -> FastAPI:
@@ -11,9 +17,13 @@ def build_app() -> FastAPI:
     tfidf_path = RESOURCES_PATH / 'baseline_tfidf.pt'
     classifier = TfidfClassifier(tfidf_path)
 
-    @app_.post('/predict', response_model=Prediction)
+    @app_.post('/predict', response_model=list[Prediction])
     async def predict_single(texts: list[str]) -> list[Prediction]:
-        return classifier.predict(texts)
+        preds = classifier.predict(texts)
+        for pred in preds:
+            enrich_theme_group(pred)
+
+        return preds
 
     return app_
 
